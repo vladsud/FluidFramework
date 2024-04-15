26f80cf27a3d7fd64f5bc9092ea8f69d017535c1
REDMOND\vladsud <vladsud@microsoft.com>
2024-04-15 07:45:55 -0700

    MatrixOt3
    (cherry picked from commit 1392f318484b4951565fd087ff32a3f7c48a9bb6)

---
+++ packages/framework/collabspaces/src/factory.ts
@@ -0,0 +1,111 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import {
+	IFluidDataStoreChannel,
+	IFluidDataStoreFactory,
+	NamedFluidDataStoreRegistryEntries,
+	IFluidDataStoreContext,
+	NamedFluidDataStoreRegistryEntry,
+} from "@fluidframework/runtime-definitions";
+import { ChannelCollectionFactory, ChannelCollection } from "@fluidframework/container-runtime";
+import { FluidDataStoreRuntime } from "@fluidframework/datastore";
+import { IChannelFactory, IFluidDataStoreRuntime } from "@fluidframework/datastore-definitions";
+import { SharedMatrixFactory, SharedMatrix } from "@fluidframework/matrix";
+import { DeferredChannelFactory } from "./deferreChannel.js";
+
+// import { DeferredChannel, DeferredChannelFactory } from "./deferreChannel";
+
+import { CollabSpacesRuntime } from "./collabSpaces.js";
+import { IEfficientMatrix, IEfficientMatrixTest } from "./contracts.js";
+
+export class MatrixDataStoreFactory implements IFluidDataStoreFactory {
+	public static readonly type = "__matrixType";
+	public readonly type = MatrixDataStoreFactory.type;
+
+	public get IFluidDataStoreFactory() {
+		return this;
+	}
+
+	public static get registryEntry(): NamedFluidDataStoreRegistryEntry {
+		return [this.type, Promise.resolve(new MatrixDataStoreFactory())];
+	}
+
+	public async instantiateDataStore(
+		context: IFluidDataStoreContext,
+		existing: boolean,
+	): Promise<FluidDataStoreRuntime> {
+		const matrixF = new SharedMatrixFactory();
+		const dataTypes = new Map<string, IChannelFactory>();
+		dataTypes.set(matrixF.type, matrixF);
+
+		const matrixDdsId = "matrix";
+
+		const runtime = new FluidDataStoreRuntime(
+			context,
+			dataTypes,
+			existing,
+			async (runtimeArg: IFluidDataStoreRuntime) => {
+				return runtimeArg.getChannel(matrixDdsId);
+			},
+		);
+
+		if (!existing) {
+			const matrix = runtime.createChannel(
+				matrixDdsId,
+				SharedMatrixFactory.Type,
+			) as SharedMatrix;
+
+			// Insert row/col for tracking row/col internal IDs
+			matrix.insertCols(0, 1);
+			matrix.insertRows(0, 1);
+
+			matrix.switchSetCellPolicy();
+
+			matrix.bindToContext();
+		}
+		return runtime;
+	}
+}
+
+class CollabSpacesFactory extends ChannelCollectionFactory {
+	constructor(registryEntriesArg: NamedFluidDataStoreRegistryEntries) {
+		const registryEntries: NamedFluidDataStoreRegistryEntries = [
+			...registryEntriesArg,
+			[DeferredChannelFactory.type, Promise.resolve(new DeferredChannelFactory())],
+		];
+		super(
+			[
+				...registryEntries,
+				[MatrixDataStoreFactory.type, Promise.resolve(new MatrixDataStoreFactory())],
+			],
+			async (runtime: IFluidDataStoreChannel) => {
+				return runtime as CollabSpacesRuntime satisfies IEfficientMatrix &
+					IEfficientMatrixTest;
+			},
+			(...args: ConstructorParameters<typeof ChannelCollection>) =>
+				new CollabSpacesRuntime(registryEntries, ...args),
+		);
+	}
+
+	public async instantiateDataStore(
+		context: IFluidDataStoreContext,
+		existing: boolean,
+	): Promise<IFluidDataStoreChannel> {
+		const runtime = (await super.instantiateDataStore(
+			context,
+			existing,
+		)) as CollabSpacesRuntime;
+		await runtime.initialize(existing);
+		return runtime;
+	}
+}
+
+/** @internal */
+export function createCollabSpaces(
+	registryEntries: NamedFluidDataStoreRegistryEntries,
+): IFluidDataStoreFactory {
+	return new CollabSpacesFactory(registryEntries);
+}
