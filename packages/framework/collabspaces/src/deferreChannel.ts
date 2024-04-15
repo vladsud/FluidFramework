1392f318484b4951565fd087ff32a3f7c48a9bb6
REDMOND\vladsud <vladsud@microsoft.com>
2024-04-15 07:45:55 -0700

    MatrixOt3

---
+++ packages/framework/collabspaces/src/deferreChannel.ts
@@ -0,0 +1,156 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { assert } from "@fluidframework/core-utils";
+import {
+	IFluidDataStoreContext,
+	ITelemetryContext,
+	IGarbageCollectionData,
+	IInboundSignalMessage,
+	IFluidDataStoreChannel,
+	ISummaryTreeWithStats,
+} from "@fluidframework/runtime-definitions";
+import { createSingleBlobSummary } from "@fluidframework/shared-object-base";
+import { readAndParse } from "@fluidframework/driver-utils";
+import { FluidObject, IFluidHandle, IRequest, IResponse } from "@fluidframework/core-interfaces";
+import { type ISequencedDocumentMessage } from "@fluidframework/protocol-definitions";
+import { FluidObjectHandle } from "@fluidframework/datastore";
+import { AttachState } from "@fluidframework/container-definitions";
+
+import { ICollabChannelFactory } from "./contracts";
+
+const snapshotFileName = "header";
+
+/**
+ * Deferred Channel
+ */
+export class DeferredChannel implements IFluidDataStoreChannel {
+	readonly type = DeferredChannel.Type;
+	static readonly Type = "CollabSpaceDeferredChannelType";
+
+	private ops: ISequencedDocumentMessage[] = [];
+
+	public readonly entryPoint: IFluidHandle<FluidObject>;
+	public get id() {
+		return this.dataStoreContext.id;
+	}
+
+	public getOps() {
+		return this.ops;
+	}
+
+	public constructor(protected readonly dataStoreContext: IFluidDataStoreContext) {
+		this.entryPoint = new FluidObjectHandle<FluidObject>(
+			{},
+			"",
+			this.dataStoreContext.IFluidHandleContext,
+		);
+	}
+
+	public async loadExisting(): Promise<void> {
+		const blobId = this.dataStoreContext.baseSnapshot?.blobs[snapshotFileName];
+		assert(blobId !== undefined, "deferred channel not serialized correctly");
+		this.ops = await readAndParse<ISequencedDocumentMessage[]>(
+			this.dataStoreContext.storage,
+			blobId,
+		);
+	}
+
+	public get value(): number {
+		assert(false, "should not be called");
+		return 0;
+	}
+
+	public getAttachSummary(telemetryContext?: ITelemetryContext): ISummaryTreeWithStats {
+		assert(false, "should not be called");
+	}
+
+	// TBD(PRI2): Implement?
+	// getAttachGCData?(telemetryContext?: ITelemetryContext): IGarbageCollectionData;
+
+	// TBD(PRI2): Implement?
+	public async getGCData(fullGC?: boolean): Promise<IGarbageCollectionData> {
+		return { gcNodes: {} };
+	}
+
+	public updateUsedRoutes(usedRoutes: string[]): void {}
+
+	public process(
+		message: ISequencedDocumentMessage,
+		local: boolean,
+		localOpMetadata: unknown,
+		addedOutboundReference?: (fromNodePath: string, toNodePath: string) => void,
+	): void {
+		this.ops.push(message);
+	}
+
+	public async summarize(
+		fullTree?: boolean,
+		trackState?: boolean,
+		telemetryContext?: ITelemetryContext,
+	): Promise<ISummaryTreeWithStats> {
+		return createSingleBlobSummary(snapshotFileName, JSON.stringify(this.ops));
+	}
+
+	/* Noop or non-callable methods */
+
+	public processSignal(message: IInboundSignalMessage, local: boolean): void {
+		assert(false, "should not be called");
+	}
+
+	public dispose() {}
+
+	public get disposed(): boolean {
+		assert(false, "should not be called");
+		return false;
+	}
+	public makeVisibleAndAttachGraph() {
+		this.dataStoreContext.makeLocallyVisible();
+	}
+
+	public setConnectionState(connected: boolean, clientId?: string) {}
+
+	public reSubmit(type: string, content: any, localOpMetadata: unknown) {
+		assert(false, "should not be called");
+	}
+
+	public async applyStashedOp(content: any): Promise<unknown> {
+		assert(false, "should not be called");
+	}
+
+	public async request(request: IRequest): Promise<IResponse> {
+		assert(false, "should not be called");
+	}
+
+	public setAttachState(attachState: AttachState.Attaching | AttachState.Attached): void {}
+}
+
+/**
+ * Deferred Channel Factory
+ */
+export class DeferredChannelFactory implements ICollabChannelFactory {
+	get IFluidDataStoreFactory() {
+		return this;
+	}
+
+	public static readonly type = DeferredChannel.Type;
+	public readonly type = DeferredChannel.Type;
+
+	public async instantiateDataStore(
+		context: IFluidDataStoreContext,
+		existing: boolean,
+	): Promise<IFluidDataStoreChannel> {
+		const channel = new DeferredChannel(context);
+		assert(existing, "existing");
+		await channel.loadExisting();
+		return channel;
+	}
+
+	public async create2(context: IFluidDataStoreContext, initialValue: unknown) {
+		assert(initialValue === undefined, "initial value");
+		const channel = new DeferredChannel(context);
+		return channel;
+	}
+}
---
+++ packages/framework/collabspaces/src/test/counterFactory.ts
@@ -0,0 +1,83 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { assert } from "@fluidframework/core-utils";
+import { SharedCounter } from "@fluidframework/counter";
+import { type IFluidDataStoreRuntime } from "@fluidframework/datastore-definitions";
+import { DataObject, DataObjectFactory, IDataObjectProps } from "@fluidframework/aqueduct";
+import {
+	IFluidDataStoreContext,
+	IFluidDataStoreChannel,
+} from "@fluidframework/runtime-definitions";
+
+import { ICollabChannel, ICollabChannelFactory } from "../contracts";
+
+export interface ISharedCounter extends ICollabChannel {
+	value: number;
+	readonly ICollabChannel: ICollabChannel;
+	increment(incrementAmount: number): void;
+}
+
+class TestDataObjectFactory
+	extends DataObjectFactory<TestDataObject>
+	implements ICollabChannelFactory
+{
+	async create2(context: IFluidDataStoreContext, initialValue: unknown) {
+		const channel = (await this.instantiateDataStore(
+			context,
+			false /* existing */,
+		)) as IFluidDataStoreRuntime & IFluidDataStoreChannel;
+
+		const counter = (await channel.getChannel("counter")) as SharedCounter;
+		counter.increment(initialValue as number);
+
+		return channel;
+	}
+}
+
+export class TestDataObject extends DataObject implements ISharedCounter {
+	public static readonly Type = "sameple-test-data-object";
+
+	public static readonly factory = new TestDataObjectFactory(
+		TestDataObject.Type,
+		TestDataObject,
+		[SharedCounter.getFactory()],
+		{},
+	);
+
+	private _counter?: SharedCounter;
+
+	public get ICollabChannel() {
+		return this;
+	}
+
+	public get value(): number {
+		assert(this._counter !== undefined, "");
+		return this._counter.value;
+	}
+
+	increment(incrementAmount: number): void {
+		assert(this._counter !== undefined, "");
+		return this._counter.increment(incrementAmount);
+	}
+
+	public async initializingFirstTime(props?: any): Promise<void> {
+		const counter = new SharedCounter(
+			"counter",
+			this.runtime,
+			SharedCounter.getFactory().attributes,
+		);
+		this.runtime.addChannel(counter);
+		counter.bindToContext();
+	}
+
+	public async hasInitialized(): Promise<void> {
+		this._counter = (await this.runtime.getChannel("counter")) as SharedCounter;
+	}
+
+	constructor(props: IDataObjectProps) {
+		super(props);
+	}
+}
